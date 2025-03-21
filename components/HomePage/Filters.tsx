59e218f37ac96741495f2febb6eaaafefe6720b8
Ucha <uchakobakhidze@gmail.com>
2025-03-21 21:18:05 +0400

    made employee filter single select instead of multi select

--- components/HomePage/Filters.tsx
+++ components/HomePage/Filters.tsx
@@ -13,8 +13,13 @@ interface FiltersProps {
 
 export default function Filters({ departments, priorities, employees }: FiltersProps) {
     const [openFilter, setOpenFilter] = useState<string | null>(null);
-
+    const [selectedEmployee, setSelectedEmployee] = useState<number | null>(null);
     const filterRef = useRef<HTMLUListElement>(null);
+
+    const handleCheckboxChange = (employeeId: number) => {
+        setSelectedEmployee((prev) => (prev === employeeId ? null : employeeId));
+    };
+
 
     const toggleFilter = (e: React.MouseEvent<HTMLLIElement>, filter: string) => {
         if (!(e.target as HTMLElement).classList.contains("filter-item")) {
@@ -162,30 +167,47 @@ export default function Filters({ departments, priorities, employees }: FiltersP
                         <path d="M6.70711 8.29289C6.31658 7.90237 5.68342 7.90237 5.29289 8.29289C4.90237 8.68342 4.90237 9.31658 5.29289 9.70711L11.2929 15.7071C11.6834 16.0976 12.3166 16.0976 12.7071 15.7071L18.7071 9.70711C19.0976 9.31658 19.0976 8.68342 18.7071 8.29289C18.3166 7.90237 17.6834 7.90237 17.2929 8.29289L12 13.5858L6.70711 8.29289Z" fill="currentColor" />
                     </svg>
                     {openFilter === "employee" && (
-                        <form className="absolute top-[120%] left-0 cursor-default bg-white border rounded-[10px] py-10 px-[1.875rem] border-primary z-10 flex flex-col w-max text-[initial] gap-[22px]" onSubmit={(e) => handleFilter(e, 'employees')}>
-                            {employees.map((employee) => {
-                                return (
-                                    <div key={employee.id} className="flex flex-row-reverse w-max gap-[15px] items-center">
-                                        <label htmlFor={`employee-${employee.id}`} className="w-max cursor-pointer flex items-center gap-[10px]">
-                                            <div className="w-[38px] h-[38px] rounded-full overflow-hidden">
-                                                <Image
-                                                    src={employee.avatar}
-                                                    alt="Employee Avatar"
-                                                    width={38}
-                                                    height={38}
-                                                    quality={100}
-                                                    priority
-                                                    className="object-cover w-full h-full"
-                                                />
-                                            </div>
-                                            {employee.name + " " + employee.surname}
-                                        </label>
-                                        <input type="checkbox" id={`employee-${employee.id}`} name="employee" value={employee.id} className="cursor-pointer" defaultChecked={isChecked('employees', employee.id.toString())}/>
+                        <form
+                        className="absolute top-[120%] left-0 cursor-default bg-white border rounded-[10px] py-10 px-[1.875rem] border-primary z-10 flex flex-col w-max text-[initial] gap-[22px]"
+                        onSubmit={(e) => handleFilter(e, "employees")}
+                    >
+                        {employees.map((employee) => (
+                            <div
+                                key={employee.id}
+                                className="flex flex-row-reverse w-max gap-[15px] items-center"
+                            >
+                                <label
+                                    htmlFor={`employee-${employee.id}`}
+                                    className="w-max cursor-pointer flex items-center gap-[10px]"
+                                >
+                                    <div className="w-[38px] h-[38px] rounded-full overflow-hidden">
+                                        <Image
+                                            src={employee.avatar}
+                                            alt="Employee Avatar"
+                                            width={38}
+                                            height={38}
+                                            quality={100}
+                                            priority
+                                            className="object-cover w-full h-full"
+                                        />
                                     </div>
-                                );
-                            })}
-                            <button className="bg-primary rounded-[20px] text-white py-2 px-[3.125rem] ml-auto mt-6">არჩევა</button>
-                        </form>
+                                    {employee.name + " " + employee.surname}
+                                </label>
+                                <input
+                                    type="checkbox"
+                                    id={`employee-${employee.id}`}
+                                    name="employee"
+                                    value={employee.id}
+                                    className="cursor-pointer"
+                                    checked={selectedEmployee === employee.id}
+                                    onChange={() => handleCheckboxChange(employee.id)}
+                                />
+                            </div>
+                        ))}
+                        <button className="bg-primary rounded-[20px] text-white py-2 px-[3.125rem] ml-auto mt-6">
+                            არჩევა
+                        </button>
+                    </form>
                     )}
                 </li>
             </ul>
